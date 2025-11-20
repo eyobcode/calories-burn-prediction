9b34bdc1fbd07c20e18adc207d05fea572a9f237
eyobScript <eyobbmulugeta@gmail.com>
2025-11-21 01:46:23 +0300

    Responsible for Combine Preprocessor + Model classes, Run training loop,Track loss, Evaluate performance

---
+++ calorie-predictor-ML/src/trainer.py
@@ -0,0 +1,18 @@
+from src.model import LinearRegressionGD
+from src.utils import mean_squared_error
+
+class Trainer:
+    def __init__(self, x, y):
+        self.X = x
+        self.y = y
+        self.model = LinearRegressionGD()
+
+    def train(self, n_iters=1000, lr=0.01):
+        self.model.lr = lr
+        self.model.n_iters = n_iters
+        self.model.fit(self.X, self.y)
+
+    def evaluate(self, x_test, y_test):
+        y_pred = self.model.predict(x_test)
+        mse = mean_squared_error(y_test, y_pred)
+        return mse
