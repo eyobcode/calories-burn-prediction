8bd7da078e1a3e7242eba07ab4e5036a0a004822
eyobScript <eyobbmulugeta@gmail.com>
2025-11-21 17:03:57 +0300

    Add Trainer class to handle model training and evaluation
    - Initialize LinearRegression model
    - Train model with specified learning rate and iterations
    - Evaluate model using mean squared error

--- calorie-predictor-ML/src/trainer.py
+++ calorie-predictor-ML/src/trainer.py
@@ -1,18 +1,19 @@
-from .model import LinearRegression
-from .utils import mean_squared_error
+from src.model import LinearRegression
+from src.utils import mean_squared_error
+
 
 class Trainer:
-    def __init__(self, x, y):
-        self.X = x
+    def __init__(self, X, y, verbose=False):
+        self.X = X
         self.y = y
-        self.model = LinearRegression()
+        self.model = LinearRegression(verbose=verbose)
 
     def train(self, n_iters=1000, lr=0.01):
         self.model.lr = lr
         self.model.n_iters = n_iters
         self.model.fit(self.X, self.y)
 
-    def evaluate(self, x_test, y_test):
-        y_pred = self.model.predict(x_test)
+    def evaluate(self, X_test, y_test):
+        y_pred = self.model.predict(X_test)
         mse = mean_squared_error(y_test, y_pred)
         return mse
